6b790b24e69a73904d27d8d53d70a21758f3cc0e
Mela <mschnetz@stud.hs-offenburg.de>
2019-05-16 14:40:57 +0200

    MaterialOrderModel angelegt

---
+++ MAWI_Context/Models/MaterialOrderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAWI_Context.Models
+{
+    public class MaterialOrderModel
+    {
+        public string Flag { get; set; }
+        public string Typ { get; set; }
+        public Nullable<int> Material { get; set; }
+        public int Amount { get; set; }
+        public Nullable<int> OrderId { get; set; }
+        public Nullable<int> CustomerOrderId { get; set; }
+    }
+}
